4d98501481078ac6746ccbf6d91c3a063e42c5ae
alfred0811 <4lfmtz@gmail.com>
2026-04-22 22:52:32 -0600

    Starting to add DirectX in Graphics

--- Framework/Graphics/Inc/Common.h
+++ Framework/Graphics/Inc/Common.h
@@ -2,3 +2,23 @@
 
 // external libraries
 #include <Core/Inc/Core.h>
+
+// directX11
+#include <d3d11_1.h>
+#include <d3dcompiler.h>
+
+#include <DirectXTK/Inc/CommonStates.h>
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dxguid.lib")
+
+template<class T>
+inline void SafeRelease(T*& ptr)
+{
+	if (ptr != nullptr)
+	{
+		ptr->Release();
+		ptr = nullptr;
+	}
+}
---
+++ Framework/Graphics/Src/GraphicsSystem.cpp
@@ -0,0 +1,5 @@
+#include "Precompiled.h"
+#include "GraphicsSystem.h"
+
+using namespace SPEngine;
+using namespace SPEngine::;
